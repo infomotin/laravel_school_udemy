e1505861ea6bf6f67aecca5c4fa36a005534df2d
Abdul Motin <motin.mmk.jr@gmail.com>
2021-06-26 14:51:03 +0600

    make:middleware

--- basi/app/Http/Kernel.php
+++ basi/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'age' => \App\Http\Middleware\checkAge::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
