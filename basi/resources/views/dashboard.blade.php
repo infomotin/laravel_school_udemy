aff9cfe371758d633b6341615c7b08e29dfaa05c
Abdul Motin <motin.mmk.jr@gmail.com>
2021-06-27 10:11:50 +0600

    create  database

--- basi/resources/views/dashboard.blade.php
+++ basi/resources/views/dashboard.blade.php
@@ -3,6 +3,13 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2> --}}
     </x-slot>
 
     <div class="py-12">
