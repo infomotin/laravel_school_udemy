f6f2f532f31d939a485d57fe1689d111883b2c0d
Abdul Motin <motin.mmk.jr@gmail.com>
2021-06-27 12:39:51 +0600

    model and migret

--- basi/database/migrations/2021_06_27_061048_create_categories_table.php
+++ basi/database/migrations/2021_06_27_061048_create_categories_table.php
@@ -15,6 +15,10 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('category_name');
+            $table->SoftDeletes();
+
             $table->timestamps();
         });
     }
