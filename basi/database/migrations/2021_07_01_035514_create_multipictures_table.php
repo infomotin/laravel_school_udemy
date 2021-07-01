aa087f0f9e26abfb88984a68aa181bcd94ac17af
Abdul Motin <motin.mmk.jr@gmail.com>
2021-07-01 09:55:29 +0600

    Multi image upload

---
+++ basi/database/migrations/2021_07_01_035514_create_multipictures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMultipicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('multipictures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multipictures');
+    }
+}
