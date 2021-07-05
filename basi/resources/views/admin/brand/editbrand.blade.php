f2849e18543413a183037211651c6b8d41660972
Abdul Motin <motin.mmk.jr@gmail.com>
2021-07-05 07:59:47 +0600

    adding back end to admin panel brand

--- basi/resources/views/admin/brand/editbrand.blade.php
+++ basi/resources/views/admin/brand/editbrand.blade.php
@@ -1,32 +1,32 @@
-<x-app-layout>
-    <x-slot name="header">
-        <b> Edit Brand </b>
-    </x-slot>
+@extends('admin.admin_master')
+@section('admin')
+    @if (session('success'))
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
+            <strong> {{ session('success') }}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
     <div class="py-12">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
                     <div class="card">
-                        @if (session('success'))
-                            <div class="alert alert-info alert-dismissible fade show" role="alert">
-                                <strong> {{ session('success') }}</strong>
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
-                            </div>
-                        @endif
+
                         <div class="card-header">
                             Edit Brand
                         </div>
                         <div class="card-body">
 
-                            <form action="{{ url('band/update/'.$loadEdit->id) }}" method="POST" enctype="multipart/form-data">
+                            <form action="{{ url('band/update/' . $loadEdit->id) }}" method="POST"
+                                enctype="multipart/form-data">
                                 @csrf
                                 <input type="hidden" name="old_image" value="{{ $loadEdit->brand_img }}">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1" class="m-2">Brand Name</label>
                                     <input type="text" class="form-control" id="exampleInputEmail1" name="brand_name"
-                                        aria-describedby="emailHelp" placeholder="Enter Brand Name" value="{{ $loadEdit->brand_name }}">
+                                        aria-describedby="emailHelp" placeholder="Enter Brand Name"
+                                        value="{{ $loadEdit->brand_name }}">
                                     {{-- validations part --}}
                                     @error('brand_name')
                                         <span class="text-danger">{{ $message }}</span>
@@ -47,7 +47,7 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputEmail1" class="m-2">Load Image</label>
-                                    <img src="{{ asset($loadEdit->brand_img) }}" style="height:150px; width:200px"/>
+                                    <img src="{{ asset($loadEdit->brand_img) }}" style="height:150px; width:200px" />
 
                                     {{-- validations part --}}
                                     @error('brand_img')
@@ -65,10 +65,10 @@
 
             </div>
         </div>
-        {{-- another  --}}
+        {{-- another --}}
 
 
 
 
     </div>
-</x-app-layout>
+@endsection
